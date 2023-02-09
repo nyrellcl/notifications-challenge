abd1517f76fe3d43d56be2121b6757dc637049df
nyrellcl <nyrell.leonor2000@gmail.com>
2023-02-09 13:25:02 -0800

    removed commented code

--- src/App.js
+++ src/App.js
@@ -15,16 +15,6 @@ function App() {
   const [isNotification, setIsNotification] = useState(true);
   const [viewMsg, setViewMsg] = useState(false);
 
-  /*const notificationsData = [
-    { id: 1, profile: mark, alt: "mark-picture", name: "Mark Webber", message: "reacted to your recent post My first tournament today!", time: "1m ago" },
-    { id: 2, profile: angela, alt: "angela-picture", name: "Angela Gray", message: "followed you", time: "5m ago" },
-    { id: 3, profile: jacob, alt: "jacob-picture", name: "Jacob Thompson", message: "has joined your group Chess Club", time: "1 day ago" },
-    { id: 4, profile: rizky, alt: "rizky-picture", name: "Rizky Hasanuddin", message: "sent you a private message", time: "5 days ago", comment: "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game. " },
-    { id: 5, profile: kimberly, alt: "kimberly-picture", name: "Kimberly Smith", message: "commented on your picture", time: "1 week ago" },
-    { id: 6, profile: nathan, alt: "nathan-picture", name: "Nathan Peterson", message: "reacted to your recent post 5 end-game strategies to increase your win rate", time: "2 weeks ago" },
-    { id: 7, profile: anna, alt: "anna-picture", name: "Anna Kim", message: "left the group Chess Club", time: "2 weeks ago" },
-  ];*/
-
   function handleReadMsg() {
     setNotifications(0)
   }
@@ -42,35 +32,6 @@ function App() {
       </header>
 
       <section className="notifications-container">
-
-        {/*{notificationsData.slice(0, notifications).map(notification => (
-          <article key={notification.id} className='notifications-container__card'>
-            <img src={notification.profile} className='picture' alt={notification.alt} />
-            <div className='notifications-container__card__content'>
-              <p><strong className='name'>{notification.name}</strong> {notification.message}</p>
-              <span className='time'>{notification.time}</span>
-            </div>
-          </article>
-        ))}
-
-        {notificationsData.slice(notifications).map(notification => (
-          <article key={notification.id} className='notifications-container__card none'>
-            <img src={notification.profile} className='picture' alt={notification.alt} />
-            <div className='notifications-container__card__content'>
-              <p><strong className='name'>{notification.name}</strong> {notification.message}</p>
-              <span className='time'>{notification.time}</span> <button type='button' className='view-msg' onClick={() => setViewMsg(!viewMsg)}>View message</button>
-              {viewMsg && (
-              <article className='msg-box'>
-                <p>{notification.comment}
-                </p>
-              </article>
-            )}
-            </div>
-          </article>
-        ))}*/}
-
-
-
         <article className='notifications-container__card'>
             <img className="picture"src={mark} alt='mark-picture'/>
           <div className='notifications-container__card__content'>
